10d42b845dd8abfe8b2dc62d0189e733f6b77e36
emilyrocher <61313812+vitaminazinhah@users.noreply.github.com>
2023-12-13 16:04:26 -0300

    Terceiro Commit
    Implementação de interface gráfica

--- funcoes.py
+++ funcoes.py
@@ -1,10 +1,7 @@
-import geocoder
 import requests
 from datetime import datetime, timedelta
+import geocoder
 
-
-   
-#classe teste localização usuario geral
 class LocalizacaoAutomatica:
     def __init__(self):
         self.lat = None
@@ -14,57 +11,69 @@ class LocalizacaoAutomatica:
         self.pais = None
 
     def obter_localizacao(self):
-        # Obtém a localização do usuário usando o endereço IP
         localizacao = geocoder.ip('me')
-
-        # Verifica se a localização foi obtida com sucesso
         if localizacao.ok:
             self.lat, self.lon = localizacao.latlng
             self.cidade = localizacao.city
             self.estado = localizacao.state
             self.pais = localizacao.country
-
             return self.formatar_localizacao()
         else:
             return 'Não foi possível obter a localização automaticamente.'
 
     def formatar_localizacao(self):
-        return (
-            f'{self.cidade},{self.estado},{self.pais}'
-        )
+        return f'{self.cidade},{self.estado},{self.pais}'
+
     def obter_cidade(self):
         return f"{self.cidade}"
+
     def obter_estado(self):
         return f"{self.estado}"
+
     def obter_pais(self):
         return f"{self.estado}"
+
     def obter_lat(self):
         return f"{self.lat}"
+
     def obter_lon(self):
         return f"{self.lon}"
-    
-    
+
+
 class WeatherDataCollector:
     def __init__(self, api_key):
         self.api_key = api_key
 
     def collect_weather_data(self, city):
-        # Endpoint da OpenWeatherMap API para previsão atual
         endpoint = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={self.api_key}&units=metric"
-
         try:
-            # Fazendo a requisição à API
             response = requests.get(endpoint)
-            response.raise_for_status()  # Lança uma exceção para códigos de erro HTTP
-
-            # Processa os dados da resposta JSON
+            response.raise_for_status()
             data = response.json()
             temperature = data["main"]["temp"]
             conditions = data["weather"][0]["description"]
-
             return {"temperature": temperature, "conditions": conditions}
         except requests.exceptions.RequestException as e:
             print(f"Erro ao coletar dados: {e}")
+            return None
+
+
+class HistoricalWeatherDataFetcher:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.base_url = 'http://api.openweathermap.org/data/2.5/onecall/timemachine'
+
+    def collect_historical_weather(self, lat, lon, date):
+        url = f'{self.base_url}?lat={lat}&lon={lon}&dt={date}&appid={self.api_key}'
+        try:
+            response = requests.get(url)
+            response.raise_for_status()
+            data = response.json()
+            temperature = data["current"]["temp"]
+            conditions = data["current"]["weather"][0]["description"]
+            return {"temperature": temperature, "conditions": conditions}
+        except requests.exceptions.RequestException as e:
+            print(f"Erro ao coletar dados históricos: {e}")
             return None
 
 
@@ -75,49 +84,54 @@ class WeeklyForecastDataFetcher:
 
     def collect_weekly_forecast(self, city, country):
         url = f'{self.base_url}?q={city},{country}&appid={self.api_key}'
-
         try:
             response = requests.get(url)
             response.raise_for_status()
-
             data = response.json()
-
-            # Filtra os dados para obter apenas a previsão para os próximos 5 dias
             daily_forecasts = [forecast for forecast in data['list'] if forecast['dt_txt'].split()[1] == '12:00:00']
-
             weekly_forecast = [
                 {
                     "date": datetime.fromisoformat(forecast['dt_txt']),
-                    "temperature": round(forecast['main']['temp'] - 273.15),  
+                    "temperature": round(forecast['main']['temp'] - 273.15),
                     "conditions": forecast['weather'][0]['description']
                 }
                 for forecast in daily_forecasts
             ]
-
             return weekly_forecast
-
         except requests.exceptions.RequestException as e:
             print(f"Erro ao coletar dados: {e}")
             return None
 
 
+class WeeklyForecast:
+    def __init__(self, weekly_forecast_data):
+        self.weekly_forecast_data = weekly_forecast_data
+
+    @staticmethod
+    def get_weekly_forecast(data_fetcher, city, country):
+        weekly_forecast_data = data_fetcher.collect_weekly_forecast(city, country)
+        return WeeklyForecast(weekly_forecast_data)
+
+
+class WeatherInfo:
+    def __init__(self, api_key):
+        self.api_key = api_key
+        self.weather_collector = WeatherDataCollector(self.api_key)
+        self.historical_data_fetcher = HistoricalWeatherDataFetcher(self.api_key)
+        self.weekly_data_fetcher = WeeklyForecastDataFetcher(self.api_key)
+
+    def get_current_weather(self, city):
+        return self.weather_collector.collect_weather_data(city)
+
+    def get_historical_weather(self, lat, lon, date):
+        return self.historical_data_fetcher.collect_historical_weather(lat, lon, date)
+
+    def get_weekly_forecast(self, city, country):
+        return WeeklyForecast.get_weekly_forecast
+
 
 class WeeklyForecast:
-    def __init__(self, data_fetcher, city, country):
-        self.data_fetcher = data_fetcher
-        self.city = city
-        self.country = country
-        self.weekly_forecast = None
-
-    def update_forecast(self):
-        # Coleta a previsão para os próximos 5 dias usando o WeeklyForecastDataFetcher
-        self.weekly_forecast = self.data_fetcher.collect_weekly_forecast(self.city, self.country)
-
-    def display_forecast(self):
-        if self.weekly_forecast:
-            print("Previsão para os Próximos 5 Dias:")
-            for day in self.weekly_forecast:
-                print(f"Data: {day['date']}, Temperatura: {day['temperature']}°C, Condição: {day['conditions']}")
-        else:
-            print("Falha ao obter a previsão para os próximos 5 dias.")
-
+    @staticmethod
+    def get_weekly_forecast(data_fetcher, city, country):
+        weekly_forecast_data = data_fetcher.collect_weekly_forecast(city, country)
+        return weekly_forecast_data
--- main.py
+++ main.py
@@ -1,78 +1,164 @@
-import requests
-import geocoder
-from tkinter import *
-
 import tkinter as tk
+from tkinter import messagebox
 import webbrowser
 import folium
+from datetime import datetime, timedelta
+from funcoes import LocalizacaoAutomatica, WeatherDataCollector, WeeklyForecastDataFetcher, WeeklyForecast, HistoricalWeatherDataFetcher
 
-from funcoes import LocalizacaoAutomatica, WeatherDataCollector, WeeklyForecastDataFetcher, WeeklyForecast
 
-def obter_localizacao_automatica():
-    localizacao = geocoder.ip('me')
+class ResultWindow:
+    def __init__(self, parent, city_data, weekly_forecast_data):
+        self.result_window = tk.Toplevel(parent)
+        self.result_window.title("Resultados da Busca")
 
-    if localizacao.ok:
-        lat, lon = localizacao.latlng
-        cidade = localizacao.city
-        estado = localizacao.state
-        pais = localizacao.country
+        if city_data:
+          
+            tk.Label(self.result_window, text=f"Temperatura Atual: {city_data['temperature']}°C").pack(pady=12)
+            tk.Label(self.result_window, text=f"Condições Atuais: {city_data['conditions']}").pack(pady=12)
 
-        print(f'{lat},{lon}')
-        #print(f'Cidade: {cidade}, Estado: {estado}, País: {pais}')
-    else:
-        print('Não foi possível obter a localização automaticamente.')
+            tk.Label(self.result_window, text="Previsão para os Próximos 5 Dias:", font=('Helvetica', 12, 'bold')).pack(pady=10)
+
+            if weekly_forecast_data:
+                for day in weekly_forecast_data:
+                    forecast_text = f"{day['date'].strftime('%Y-%m-%d')}: {day['temperature']}°C, {day['conditions']}"
+                    tk.Label(self.result_window, text=forecast_text, font=('Helvetica', 10)).pack(anchor='w')
+            else:
+                tk.Label(self.result_window, text="Falha ao obter a previsão para os próximos 5 dias.", font=('Helvetica', 10)).pack(pady=10)
+        else:
+            tk.Label(self.result_window, text="Erro ao obter dados da cidade.", font=('Helvetica', 10)).pack(pady=10)
+
+
+class WeatherApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Consulta Meteorológica")
+
+        localizacao_obj = LocalizacaoAutomatica()
+        resultado = localizacao_obj.obter_localizacao()
+        self.city_usuario = localizacao_obj.obter_cidade()
+        self.country_usuario = localizacao_obj.obter_pais()
+        self.lat_usuario = localizacao_obj.obter_lat()
+        self.lon_usuario = localizacao_obj.obter_lon()
+
+        self.api_key = '95a2aebd33fc348608c9f6d3ace9c548'
+        self.weather_collector = WeatherDataCollector(self.api_key)
+        self.city_data = self.weather_collector.collect_weather_data(f"{self.city_usuario},{self.country_usuario}")
+
+        # Obtém a previsão para os próximos cinco dias
+        self.weekly_data_fetcher = WeeklyForecastDataFetcher(self.api_key)
+        self.weekly_forecast_data = WeeklyForecast.get_weekly_forecast(self.weekly_data_fetcher, self.city_usuario, self.country_usuario)
+
+        # Criação da interface gráfica
+        self.create_widgets()
+
+    def create_widgets(self):
+        # Estilo para deixar a interface mais agradável
+        self.root.geometry("400x550")  # Ajustei a altura para acomodar o novo campo
+        self.root.configure(bg='#E5E5E5')  # Cor de fundo
+        self.root.resizable(False, False)  # Torna a janela não redimensionável
+
+        # Informações climáticas atuais
+        self.lbl_city = tk.Label(self.root, text=f"Localização Atual: {self.city_usuario}", font=('Helvetica', 12, 'bold'), bg='#E5E5E5')
+        self.lbl_temperature = tk.Label(self.root, text="Temperatura Atual: -", font=('Helvetica', 12, 'bold'), bg='#E5E5E5')
+        self.lbl_conditions = tk.Label(self.root, text="Condições Atuais: -", font=('Helvetica', 12, 'bold'), bg='#E5E5E5')
+
+        # Informações climáticas históricas
+        # self.lbl_historical_temperature = tk.Label(self.root, text=" ")
+        # self.lbl_historical_conditions = tk.Label(self.root, text=" ")
+
+       
+        self.lbl_weekly_forecast = tk.Label(self.root, text="Previsão para os Próximos 5 Dias:", font=('Helvetica', 13, 'bold'), bg='#E5E5E5')
+
+        # Container para a previsão semanal
+        self.frame_weekly_forecast = tk.Frame(self.root, bg='#E5E5E5')
+
+        # Botão para obter dados históricos
+        self.btn_get_historical_data = tk.Button(self.root, text="Obter Dados Históricos", command=self.get_historical_data, font=('Helvetica', 10), bg='#4CAF50', fg='white')
+
+        # Botão para abrir o mapa
+        self.btn_open_map = tk.Button(self.root, text="Abrir Mapa", command=self.open_map, font=('Helvetica', 10), bg='#2196F3', fg='white')
+
+        # Campo de busca
+        self.entry_city_search = tk.Entry(self.root, font=('Helvetica', 12))
+        self.entry_city_search.insert(0, "")  # Texto padrão
+        self.lbl_search_message = tk.Label(self.root, text="Buscar nova localização", font=('Helvetica', 12, 'bold'), bg='#E5E5E5')
+        self.l_search_message = tk.Label(self.root, text="Padrão: Cidade, Pais", font=('Helvetica', 8), bg='#E5E5E5')
+        # Botão de busca
+        self.btn_search = tk.Button(self.root, text="Buscar", command=self.search_city, font=('Helvetica', 10, 'bold'), bg='#FFD700')  # Cor amarela
+
+        # Posiciona os widgets
+        self.lbl_city.pack(pady=10)
+        self.lbl_temperature.pack(pady=10)
+        self.lbl_conditions.pack(pady=10)
+        self.lbl_weekly_forecast.pack(pady=10)
+        self.frame_weekly_forecast.pack(pady=10)
+        self.btn_get_historical_data.pack(pady=10)
+        self.btn_open_map.pack(pady=10)
+        self.lbl_search_message.pack(pady=5)
+        self.l_search_message.pack(pady=5)
+        self.entry_city_search.pack(pady=5)
+        self.btn_search.pack(pady=10)
+
+        # Exibe a previsão inicialmente
+        self.display_current_weather()
+        self.display_weekly_forecast()
+
+    def get_historical_data(self):
+       
+        historical_data_fetcher = HistoricalWeatherDataFetcher(self.api_key)
+        historical_date = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+        self.historical_data = historical_data_fetcher.collect_historical_weather(self.lat_usuario, self.lon_usuario, historical_date)
+
+        if self.historical_data:
+        
+            messagebox.showinfo("Dados Históricos", f"Temperatura Histórica: {self.historical_data['temperature']}°C\nCondições Históricas: {self.historical_data['conditions']}")
+        else:
+            messagebox.showerror("Erro", "Falha ao obter dados históricos.")
+
+    def display_current_weather(self):
+        if self.city_data:
+            self.lbl_temperature.config(text=f"Temperatura Atual: {self.city_data['temperature']}°C")
+            self.lbl_conditions.config(text=f"Condições Atuais: {self.city_data['conditions']}")
+        else:
+            messagebox.showerror("Erro", "Falha ao obter dados climáticos atuais.")
+
+    def display_weekly_forecast(self):
+        if self.weekly_forecast_data:
+            for day in self.weekly_forecast_data:
+                forecast_text = f"{day['date'].strftime('%Y-%m-%d')}: {day['temperature']}°C, {day['conditions']}"
+                tk.Label(self.frame_weekly_forecast, text=forecast_text, font=('Helvetica', 10), bg='#E5E5E5').pack(anchor='w')
+        else:
+            messagebox.showerror("Erro", "Falha ao obter a previsão para os próximos 5 dias.")
+
+    def open_map(self):
+        # Cria um mapa centrado na localização atual
+        mapa = folium.Map(location=[float(self.lat_usuario), float(self.lon_usuario)], zoom_start=10)
+
+        # Adiciona um marcador com informações sobre a localização
+        popup_text = f"Localização: ({self.lat_usuario}, {self.lon_usuario})\nTemperatura Atual: {self.city_data['temperature']}°C"
+        folium.Marker([float(self.lat_usuario), float(self.lon_usuario)], popup=popup_text).add_to(mapa)
+
+        # Salva o mapa em um arquivo HTML temporário
+        mapa.save("temp_mapa_meteorologico.html")
+
+        # Abre o mapa em um navegador externo
+        webbrowser.open("temp_mapa_meteorologico.html", new=2)
+
+    def search_city(self):
+       
+        city_and_country = self.entry_city_search.get()
+        city_and_country = city_and_country.split(",")  
+        if len(city_and_country) == 2:
+            self.city_usuario, self.country_usuario = map(str.strip, city_and_country)
+            self.city_data = self.weather_collector.collect_weather_data(f"{self.city_usuario},{self.country_usuario}")
+            self.weekly_forecast_data = WeeklyForecast.get_weekly_forecast(self.weekly_data_fetcher, self.city_usuario, self.country_usuario)
+
+            
+            ResultWindow(self.root, self.city_data, self.weekly_forecast_data)
+        else:
+            messagebox.showwarning("Aviso", "Por favor, insira a cidade e o país separados por vírgula.")
 
 if __name__ == "__main__":
-    obter_localizacao_automatica()
-
-    localizacao_obj = LocalizacaoAutomatica()
-    resultado = localizacao_obj.obter_localizacao()
-    city_usuario = localizacao_obj.cidade
-    country_usuario = localizacao_obj.pais
-    lat_usuario= localizacao_obj.lat
-    lon_usuario= localizacao_obj.lon
-
-    print(resultado)
-
-    api_key = '95a2aebd33fc348608c9f6d3ace9c548'
-
-    # INFORMACOES CLIMATICAS COM BASE NA LOCALIZAÇÃO E DATA ATUAL DO USUARIO
-    weather_collector = WeatherDataCollector(api_key)
-    city_data = weather_collector.collect_weather_data(resultado)
-
-    if city_data:
-        print(f"Temperatura da localização atual: {city_data['temperature']}°C")
-        print(f"Condições meteorológicas: {city_data['conditions']}")
-    else:
-        print("Falha ao coletar dados.")
-
-
-    # INFORMACOES CLIMATICAS DOS PROXIMOS 5 DIAS 
-    weekly_data_fetcher = WeeklyForecastDataFetcher(api_key)
-    weekly_forecast_instance = WeeklyForecast(weekly_data_fetcher, city_usuario, country_usuario)
-    weekly_forecast_instance.update_forecast()
-    weekly_forecast_instance.display_forecast()
-
-
-
-url = f"http://api.openweathermap.org/data/2.5/weather?lat={lat_usuario}&lon={lon_usuario}&appid={api_key}"
-
-# Crie um mapa centrado na localização
-mapa = folium.Map(location=[lat_usuario, lon_usuario], zoom_start=10)
-
-popup_text = f"Localização: ({lat_usuario}, {lon_usuario})\nTemperatura: {city_data['temperature']}°C"
-folium.Marker([lat_usuario, lon_usuario], popup=popup_text).add_to(mapa)
-
-# salvando o mapa em um arquivo HTML temporário
-mapa.save("temp_mapa_meteorologico.html")
-
-# abrir o mapa em um navegador externo
-def abrir_mapa():
-    webbrowser.open("temp_mapa_meteorologico.html", new=2)
-
-
-root = tk.Tk()
-root.title("Mapa Meteorológico")
-abrir_mapa_button = tk.Button(root, text="Abrir Mapa", command=abrir_mapa)
-abrir_mapa_button.pack(pady=10)
-root.mainloop()
+    root = tk.Tk()
+    app = WeatherApp(root)
+    root.mainloop()
